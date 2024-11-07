5692b58d5aed497466223f36d2019ef1b5862ff9
Ryan Harricharan <138528052+rharricharan@users.noreply.github.com>
2024-11-07 17:19:27 -0500

    compleing home page

--- cin-web/src/pages/Home.js
+++ cin-web/src/pages/Home.js
@@ -73,6 +73,50 @@ function Home() {
                     <ScrollAnimation video="motion4.mp4" setVideo={setVideo} setBgOpacity={setBgOpacity}>With us, every word, every call, every text is exactly as it should be: simple, dependable, and unbreakable.</ScrollAnimation>
                 </section>
             </main>
+            <div className="commit-section">
+                <div className="commit-tt-section">
+                    <h2 className="commit-title">Our Commitment to You</h2>
+                    <h3 className="commit-p">Whether you’re a growing startup or a multinational leader, we exist to make connecting as natural as a conversation across the table.</h3>
+                </div>
+                <div className="card-wrapper-com">
+                    <div className="card-com">
+                        <h3 className="top-title-com">Always Clear, Always Reliable</h3>
+                        <h3 className="bttm-title-com">Delivering uncompromising quality with 99.99% uptime.</h3>
+                        <div className="bttn-container-com">
+                            <a className="com-bttn">
+                                get started
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
+                                    <path d="M16.172 11.5L10.808 6.13605L12.222 4.72205L20 12.5L12.222 20.278L10.808 18.864L16.172 13.5H4V11.5H16.172Z" fill="#F8F8F8"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="card-com">
+                        <h3 className="top-title-com">Built for Growth, Made to Last</h3>
+                        <h3 className="bttm-title-com">Scale at your pace, without limits or inflated costs.</h3>
+                        <div className="bttn-container-com">
+                            <a className="com-bttn">
+                                get started
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
+                                    <path d="M16.172 11.5L10.808 6.13605L12.222 4.72205L20 12.5L12.222 20.278L10.808 18.864L16.172 13.5H4V11.5H16.172Z" fill="#F8F8F8"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="card-com">
+                        <h3 className="top-title-com">Seamless and Intuitive</h3>
+                        <h3 className="bttm-title-com">Technology that just works, without the technical headache.</h3>
+                        <div className="bttn-container-com">
+                            <a className="com-bttn">
+                                get started
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
+                                    <path d="M16.172 11.5L10.808 6.13605L12.222 4.72205L20 12.5L12.222 20.278L10.808 18.864L16.172 13.5H4V11.5H16.172Z" fill="#F8F8F8"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
