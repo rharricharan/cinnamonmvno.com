bdfc9095f9d6933eeb3d0ee9d223f6da1e06bead
Ryan Harricharan <138528052+rharricharan@users.noreply.github.com>
2024-11-08 20:43:18 -0500

    replacing browser routers to hash routers

--- cin-web/src/App.js
+++ cin-web/src/App.js
@@ -1,17 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
