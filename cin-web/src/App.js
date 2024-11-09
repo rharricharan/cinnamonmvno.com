b7f99d17b46ea3e199e8ee1e4cd8d2e868c51e83
Ryan Harricharan <138528052+rharricharan@users.noreply.github.com>
2024-11-09 13:41:59 -0500

    reverting gh-pages back to predeployment env

--- cin-web/src/App.js
+++ cin-web/src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 
 function App() {
   return (
-    <HashRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
       </Routes>
-    </HashRouter>
+    </Router>
   );
 }
 
